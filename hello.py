eefe59f3809882545baa8ca73bb3a9396351c43d
Abdirisakhd <62589751+Abdirisakhd@users.noreply.github.com>
2020-12-19 10:11:06 +0300

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,10 @@
+# First Python code in this repository 
+
+name = input("enter your name: ")
+gender = input("enter your gender(male or female): ")
+if gender =="male":
+    print("hello mr",name,"/nWelcome to my repository..")    
+elif gender == "female":
+    print("wecome mrs",name,"/nWelcome to my repository")
+
+# feel free to change what ever you want 
