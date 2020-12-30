cb0b3eaf63ec32cc0d2e3e187279da71b3e9e928
Abdirisakhd <62589751+Abdirisakhd@users.noreply.github.com>
2020-12-30 08:26:02 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,8 +3,10 @@
 name = input("enter your name: ")
 gender = input("enter your gender(male or female): ")
 if gender =="male":
-    print("hello mr",name,"/nWelcome to my repository..")    
+    print("hello mr",name,"\nWelcome to my repository..")    
 elif gender == "female":
-    print("wecome mrs",name,"/nWelcome to my repository")
+    print("wecome mrs",name,"\nWelcome to my repository")
+else:
+    print("wrong input!!")
 
-# feel free to change what ever you want 
+# feel free to change or add what ever you want:)
